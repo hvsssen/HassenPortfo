91596c535a337c82838b151ba7e795fbd0ad14b8
hvsssen7 <hvsssen7@gmail.com>
2025-02-09 01:26:11 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from "./home/home.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ResumeComponent } from "./resume/resume.component";
+import { ProjectsComponent } from './projects/projects.component';
+import { CommonModule } from '@angular/common';
+import{SkillsComponent} from './skills/skills.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [SkillsComponent,CommonModule,RouterOutlet,NavbarComponent,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portf';
+  title = 'hassenPortfolio';
 }
